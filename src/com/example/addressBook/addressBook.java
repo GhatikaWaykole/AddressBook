1323fda2db55c30f885c54d0d1bbaa4382c5ea2c
ghatikawaykole <ghatika99@gmail.com>
2021-09-17 10:29:10 +0530

    welcome to addressbook

---
+++ src/com/example/addressBook/addressBook.java
@@ -0,0 +1,9 @@
+package com.example.addressBook;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+public class addressBook {
+    public static void main(String args[]){
+        System.out.println("Welcome to the adresbook");
+    }
+}
